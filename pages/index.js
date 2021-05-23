147c5d69953ad074dc2f4d8d04f9fee08634ee8b
Jake <jake.ord345@gmail.com>
2021-05-23 01:47:36 +0100

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stars } from "@react-three/drei";
 import Content from "../components/Content";
-import Stars from "../components/Stars";
 import Sphere from "../components/Sphere";
 import World from "../components/World";
 import Dragon from "../components/Dragon";
-import Lights from "../components/Lights";
 import { Suspense } from "react";
 
 export default function Home() {
@@ -13,7 +11,9 @@ export default function Home() {
     <>
       <Canvas camera={{ fov: 75, near: 1, far: 1000, position: [5, 5, 10] }}>
         <OrbitControls autoRotate />
-        <Lights />
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 15, 10]} angle={0.8} />
+        <fog args={["aquamarine", 1, 100]} />
         <Sphere position={[-5, 4, 3]} color="crimson" />
         <Sphere position={[-5, 4, -7]} color="teal" />
         <Sphere position={[5, 4, 3]} color="darkslateblue" />
